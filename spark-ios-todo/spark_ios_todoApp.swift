6582c648948a8ac24c1d9b4fd98a908198a648b6
Kreedzt <zhaozisong1@live.com>
2026-06-15 14:51:13 +0800

    Initial Commit

---
+++ spark-ios-todo/spark_ios_todoApp.swift
@@ -0,0 +1,32 @@
+//
+//  spark_ios_todoApp.swift
+//  spark-ios-todo
+//
+//  Created by ken on 2026/6/15.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct spark_ios_todoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
